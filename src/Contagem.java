1fec4ab69719bd8fc909465841a68d4b714d1936
Marcella Lara <marcellalaraesteves@gmail.com>
2024-06-10 10:36:44 -0300

    Exercício while contador simples

---
+++ src/Contagem.java
@@ -0,0 +1,10 @@
+public class Contagem {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador <= 100){
+            System.out.println(contador);
+            contador += 10;
+        }
+    }
+}
