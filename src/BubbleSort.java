922293c4bbdcc4fed726ae7b688113e119c19fbd
Marcella Lara <marcellalaraesteves@gmail.com>
2024-06-14 09:15:08 -0300

    Exercício bubble sort ordenações

---
+++ src/BubbleSort.java
@@ -0,0 +1,26 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] vetor = {64, 34, 25, 12, 22, 11, 90};
+
+        boolean houveTroca;
+        do{
+            houveTroca = false;
+            for (int i = 1; i < vetor.length; i++){
+                //Compara elementos adjacentes
+                if (vetor[i - 1] > vetor[i]){
+                    //Troca os elementos de lugar
+                    int temp = vetor[i - 1];
+                    vetor[i - 1] = vetor[i];
+                    vetor[i] = temp;
+                    houveTroca = true;
+                }
+            }
+        } while (houveTroca);
+
+        System.out.println("Vetor ordenado:");
+        for (int i =0; i < vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
+//Eficiente para ordenações
