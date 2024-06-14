615b63023d154772bd92044e52dd8c8ddf664618
Marcella Lara <marcellalaraesteves@gmail.com>
2024-06-14 10:12:09 -0300

    Exercício matriz cinema

---
+++ src/CinemaMatrizes.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class CinemaMatrizes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] salaCinema = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1},
+                {1, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0}
+        };
+
+        System.out.println("Situação atual dos assentos (L = Livre, O = Ocupado):");
+        for (int i = 0; i < salaCinema.length; i++){
+            for (int j = 0; j < salaCinema[i].length; j++){
+                System.out.print(salaCinema[i][j] == 0 ? "L " : "O ");
+            }
+            System.out.println(" <- Fila " + (i + 1));
+        }
+
+        //Recebe a escolha
+        System.out.println("Escolha a fila:");
+        int filaEscolhida = scanner.nextInt() - 1; //O -1 ajusta para o índice do vetor (começa em 0)
+
+        System.out.println("Escolha o número do assento:");
+        int assentoEscolhido = scanner.nextInt() - 1;
+
+        //Verifica a reserva do assento
+        if (filaEscolhida >= 0 && filaEscolhida < salaCinema.length && assentoEscolhido >= 0 && assentoEscolhido < salaCinema[filaEscolhida].length){
+            if (salaCinema[filaEscolhida][assentoEscolhido] == 0){
+                salaCinema[filaEscolhida][assentoEscolhido] = 1;
+                System.out.println("Assento reservado com sucesso");
+            }else {
+                System.out.println("O assento já está ocupado, por favor escolha outro");
+            }
+        }else {
+            System.out.println("Escolha inválida, tente novamente");
+        }
+        scanner.close();
+    }
+}
