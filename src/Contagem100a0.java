c2e602f32bfc3eb8b653320f8b838fdeca4f89e6
marcellaesteves <marcella.almeida@youxlab.com.br>
2024-06-27 16:42:44 -0300

    Exercício contador 100 a 0 while

---
+++ src/Contagem100a0.java
@@ -0,0 +1,11 @@
+public class Contagem100a0 {
+    public static void main(String[] args) {
+        int i = 100;
+
+        while (i != -10){
+            System.out.print(i + " ");
+            i -= 10;
+        }
+        System.out.println(" Acabou!");
+    }
+}
