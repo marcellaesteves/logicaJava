c6aab7e7212446eed3c7562c28c94b60a43aad0f
marcellaesteves <marcella.almeida@youxlab.com.br>
2024-06-27 16:35:42 -0300

    Exercício contador while

---
+++ src/ContadorWhile.java
@@ -0,0 +1,11 @@
+public class ContadorWhile {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i != 45){
+            System.out.print(i + " ");
+            i += 5;
+        }
+        System.out.print(" Acabou!");
+    }
+}
